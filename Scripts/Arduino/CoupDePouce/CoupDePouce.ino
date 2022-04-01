e213953a3f3f6bb3695d2ccb4602daabebaf9e20
Simon Moinard <simmoinard@gmail.com>
2022-04-01 16:02:37 +0200

    Update CoupDePouce.ino

--- Scripts/Arduino/CoupDePouce/CoupDePouce.ino
+++ Scripts/Arduino/CoupDePouce/CoupDePouce.ino
@@ -1,17 +1,37 @@
-/* Ce code permet d'activer un Servo Moteur et appuie sur un bouton pour lancer la pompe d'irrigation
- * L'envoi du SMS "1" permet d'activer ce système.
- * Ce code nécessite un MKR1400 ainsi qu'un Servo moteur
- */
+/*
+  CoupDePouce.ino
+
+  Activate a stepmoter through SMS Message. Circuit needs an Arduino MKRWANGSM 1400 and a stepper motor 28byj-48 with ULN2003 driver
+  Activer un moteur a distance via l'envoi de SMS. Le montage nécessite une carte Arduino MKRWAN GSM 1400 ainsi qu'un moteur pas à pas 28byj-48 with ULN2003 driver
+
+  The circuit:
+
+** - <-> pin GND
+** + <-> pin VCC
+
+** IN1 <-> pin 6
+** IN2 <-> pin 7
+** IN3 <-> pin 8
+** IN4 <-> pin 9
+
+  Createad 25 March 2019
+  modified 01 April 2022
+  v3.1
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+*/
 
 #include <MKRGSM.h>
 #include <Servo.h>
 #include "arduino_secrets.h" 
 
 const char PINNUMBER[] = SECRET_PINNUMBER;
-const char Alex[] = ALEX;
-const char Denis[] = DENIS;
-const char Max[] = MAX;
-const char Simon[] = SIMON;
+const char num1[] = num1;
+const char num2[] = num2;
+
+//Rajoutez une ligne ici pour rajouter un numéro de téléphone : const char num3[] = num3;
 
 char senderNumber[20];
 
@@ -29,10 +49,10 @@ void setup() {
     if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
       connected = true;
     } else {
-      Serial.println("probleme initialisation");
+      Serial.println("probleme lors de l'initialisation GSM. La carte SIM est-elle bien en place ? Le code PIN est-t-il bien rempli dans le fichier arduino_secrets.h ? ");
     }
   }
-  Serial.println("GSM initialized");
+  Serial.println("GSM intilialisé");
   char iniMsg[] = "Arduino OK : envoyer 1 pour allumer la pompe";
   envoiSMS(Simon, iniMsg);  
   envoiSMS(Alex, iniMsg);
